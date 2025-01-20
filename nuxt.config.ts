0f2037a2154799ec780ab12ddd82761fcc1ed964
rich-tribeweb <rich@tribeweb.co.uk>
2025-01-20 13:37:11 +0000

    feat: add nuxthub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/ui-pro', // @nuxt ui-pro must go BEFORE @nuxt/content for prose to work in .md files
-    '@nuxt/content'
-  ],
+  modules: [// @nuxt ui-pro must go BEFORE @nuxt/content for prose to work in .md files
+  '@nuxt/ui-pro', '@nuxt/content', '@nuxthub/core'],
 
   runtimeConfig: {
     // Private keys are only available on the server
@@ -16,6 +14,12 @@ export default defineNuxtConfig({
 // components: [ // don't just leave this empty or it fails to find any components!
 //   { path: '~/components/content', prefix: 'Content' }, // not sure this actually does anything
 // ],
+
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
 
   future: {
     compatibilityVersion: 4
